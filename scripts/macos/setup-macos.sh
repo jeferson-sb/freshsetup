2e2988a6305d161ba9bdcad69f48e7d4e44bff06
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2026-01-24 17:15:49 -0300

    add macos defaults and brew apps

--- scripts/macos/setup-macos.sh
+++ scripts/macos/setup-macos.sh
@@ -5,15 +5,32 @@ function install_homebrew() {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
+function update_all() {
+  next "update packages"
+  brew update
+  check "Your system is up-to-date"
+}
+
+function sane_defaults() {
+  next "prettify macOS"
+  next "mouse scroll"
+  defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+  next "finder"
+  defaults write com.apple.finder AppleShowAllFiles -bool true
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+  defaults write com.apple.finder ShowPathbar -bool true
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+  defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+  check "System is ready"
+}
+
 # Essentials
 install_homebrew
 
-# Update packages
-next "update packages"
-brew update
-check "Your system is up-to-date"
+# Update system upfront
+update_all
 
-# Essentials
+# Needed for dev
 brew install coreutils curl git
 brew install gpg gawk
 
@@ -24,13 +41,18 @@ brew install gh
 brew install powerlevel10k
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
-brew install --cask google-chrome
-brew install --cask iterm2
+brew install --cask google-chrome warp visual-studio-code spotify discord bruno
 
 # Xcode
 xcode-select --install
 
+# Dev packages
+# TBD
+
+# Macos sane defaults
+sane_defaults()
+
 # Folders
-mkdir projects && cd projects && mkdir personal oss work
+[[ ! -d "$HOME/projects" ]] && mkdir $HOME/projects && cd $HOME/projects && mkdir personal oss work
 
 check "Your system is ready!"
